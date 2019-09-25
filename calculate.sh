25d91ab826441ec83e22e14ba90ef84bbf3f735d
Matthew Wong <m2wong@oicr.on.ca>
2019-09-25 12:37:25 -0400

    ignore logs when doing md5 comparison

--- calculate.sh
+++ calculate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 find . -name "*.zip" -exec unzip {} \; >/dev/null
-find . -type f -not -path "./*.zip" -exec md5sum {} + 
+find . -type f -not -path "./*.zip" -not -name "*.log" -exec md5sum {} + 
